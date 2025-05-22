333267e643ed3f66dd9c3bf94a225ec2ce0f381a
JP-fras <frascinojuanpablo@gmail.com>
2025-05-22 14:04:38 -0300

    merge con raigo 2

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,27 +1,19 @@
 
 import HeroSection from '../components/sections/HeroSection';
 import PhotoUploader from '../components/sections/PhotoUploader';
-<<<<<<< HEAD
-//import FeaturesSection from '../components/sections/FeaturesSection';
-import FaqSection from '../components/sections/FaqSection';
-=======
 import FeaturesSection from '../components/sections/FeaturesSection'
 import FaqSection from '../components/sections/FaqSection'
->>>>>>> origin/raigo
 
 function Inicio() {
   return (
     <div>
-<<<<<<< HEAD
       <div id="inicio"><HeroSection /></div>
       <div id="photo"><PhotoUploader/></div>
       <div id="faq"><FaqSection/></div>
-=======
       <HeroSection />
       <PhotoUploader />
       <FaqSection/>
       <FeaturesSection/>
->>>>>>> origin/raigo
     </div>
   );
 }
